01646c6172f0a11c2e5f1cd2bde2f47c99850e9a
exlabbu <exlabbu@gmail.com>
2022-10-23 15:09:32 +0200

    Zadanie 8

--- grupa zadan/zadanie 8.py
+++ grupa zadan/zadanie 8.py
@@ -1,4 +1,3 @@
-from random import Random
 import random
 
 
@@ -12,25 +11,65 @@ def inuptValue():
         else:
             if (a <= 0):
                 continue
+            elif(a == 1):
+                print("X")
+                print("U")
+                exit()
             break
     return a
 
-def doXmasTree(a):
-    i = 0
-    l = 1
-    for x in range(2,a-1):
-        l+=2
-    print(l)
-    j = 1
-    tab = "\t"
-    while(j == ((l-1)/2)):
-        tab = str(tab)+" \t"
-        j+=1
-    print(tab+"X")
-    while(i != a):
-        
-        random.randint(2,a-1)
+class XmasTree:
 
-        # Dopisać resztę choinki!
+    def __init__(self,a):
+        # a-1 to odległość jaką mam do pnia z lewej strony
+        self.toTheTrunk = a-1
+        self.height = a
 
-doXmasTree(5)
+    def countSapce(self,lev):
+        # przyjmuje że level to aktualny poziom liczony od 1
+        s = ""
+        for x in range(1,self.toTheTrunk - (lev-1)):
+            s = s + " "
+        return s
+
+    def setStarOnTheTop(self):
+        print(self.countSapce(1)+"X")
+
+    def makeStandUnderTree(self):
+        print(self.countSapce(1)+"U")
+
+    def makeTreeWithXmasTreeBaubles(self, lev):
+        # parametrem jest level początkowy
+        def countXmasWidth(lev):
+            def randomizeTreeBoubles(b):
+                s = ""
+                i = 0
+                while(i != b):
+                    if(random.randint(0,b*2) == 0):
+                        s = s + "o"
+                    else:
+                        s = s + "*"
+                    i+=1
+                return s
+
+
+            s = randomizeTreeBoubles(3)
+            for x in range(1,lev-1):
+                s = s + randomizeTreeBoubles(2)
+            return s
+        while (lev != self.height):
+            print(self.countSapce(lev)+countXmasWidth(lev))
+            lev += 1
+
+    def setTree(self):
+        self.setStarOnTheTop()
+        self.makeTreeWithXmasTreeBaubles(2)
+        self.makeStandUnderTree()
+
+
+
+    
+    
+
+obj = XmasTree(inuptValue())
+obj.setTree()
